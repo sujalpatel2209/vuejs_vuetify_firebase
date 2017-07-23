cf9e392bef45cc9534bdf036cd277340509a57af
sujalpatel2209 <sujalpatel022@gmail.com>
2017-07-23 14:37:03 +0530

    Setup Vuejs-cli and vuetify

---
+++ pages/CustomView.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-layout column justify-center>
+    <img src="/static/v.png" alt="Vuetify.js" class="mb-5" />
+    <blockquote>
+      Custom Vue and Routing
+      <footer>
+        <small>
+          <em>&mdash;Sujal Patel</em>
+        </small>
+      </footer>
+    </blockquote>
+  </v-layout>
+</template>
