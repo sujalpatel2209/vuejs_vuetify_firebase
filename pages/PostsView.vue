9fb4fb4913025fa7e946db631cf2b18cc6713e7d
sujalpatel2209 <sujalpatel022@gmail.com>
2017-08-20 12:31:58 +0530

    ~ implement vux ~ Define posts array in store state ~ Store state fetch in posts page

--- pages/PostsView.vue
+++ pages/PostsView.vue
@@ -1,13 +1,13 @@
 <template>
   <v-container>
-    <v-layout>
+    <v-layout v-for="post in posts" :key="post.id" class="mb-2">
       <v-flex xs12 sm10 md8 offset-sm1 offset-md2>
         <v-card class="info">
           <v-container fluid>
             <v-layout row>
               <v-flex xs5 sm4 md3>
                 <v-card-media
-                  src="static/slider/slider1.png"
+                  :src="'static/slider/'+ post.imageName"
                   height="150px"
                 >
                 </v-card-media>
@@ -15,12 +15,12 @@
               <v-flex xs7 sm8 md9>
                 <v-card-title>
                   <div>
-                    <h5 class="white--text">Post Title</h5>
-                    <div>2017-06-22</div>
+                    <h5 class="white--text">{{ post.title }}</h5>
+                    <div>{{ post.date }}</div>
                   </div>
                 </v-card-title>
                 <v-card-action>
-                  <v-btn flat to="/posts/1">
+                  <v-btn flat :to="'/posts/'+post.id">
                     <v-icon left light class="white--text">arrow_forward</v-icon>
                     View Post
                   </v-btn>
@@ -33,3 +33,12 @@
     </v-layout>
   </v-container>
 </template>
+<script>
+  export default {
+    computed :{
+      posts() {
+        return this.$store.state.posts
+      }
+    }
+  }
+</script>
--- store/index.js
+++ store/index.js
@@ -5,8 +5,24 @@ Vue.use(Vuex)
 
 export function createStore () {
   return new Vuex.Store({
-    state: {},
-
+    state: {
+      posts: [
+        {
+          id: 'P1',
+          title: 'First Post',
+          date: '2017-06-22',
+          content: 'Post Content',
+          imageName: 'slider1.png'
+        },
+        {
+          id: 'P2',
+          title: 'Second Post',
+          date: '2017-06-23',
+          content: 'second Post Content',
+          imageName: 'slider2.jpg'
+        }
+      ]
+    },
     actions: {},
 
     mutations: {},
