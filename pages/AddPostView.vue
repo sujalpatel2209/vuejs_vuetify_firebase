f61a0989f21a0d8cf7990c60f8d9afe186e23f00
sujalpatel2209 <sujalpatel022@gmail.com>
2017-08-22 22:32:27 +0530

    ~ Create Add post form

--- pages/AddPostView.vue
+++ pages/AddPostView.vue
@@ -1,3 +1,84 @@
 <template>
-  <h1>Add Post</h1>
+  <v-container>
+    <v-layout row>
+      <v-flex xs12 sm6 offset-sm3>
+        <h4  class="white--text">Create Post</h4>
+      </v-flex>
+    </v-layout>
+    <v-layout row>
+      <v-flex xs12>
+        <form>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-text-field
+              name="title"
+              label="Title"
+              id="title"
+              v-model="title"
+              required>
+              </v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-text-field
+                name="setDate"
+                label="Set Date"
+                id="setDate"
+                v-model="setDate"
+                required>
+              </v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-text-field
+                name="imageUrl"
+                label="Image URL"
+                id="imageUrl"
+                v-model="imageUrl"
+                required>
+              </v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <img :src="imageUrl" height="150">
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-text-field
+                name="description"
+                label="Description"
+                id="description"
+                v-model="description"
+                multi-line
+                required>
+              </v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-btn class="primary">
+                Create Post
+              </v-btn>
+            </v-flex>
+          </v-layout>
+        </form>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
+<script>
+  export default {
+    data(){
+      return{
+        title:'',
+        setDate: '',
+        imageUrl: '',
+        description: ''
+      }
+    }
+  }
+</script>
